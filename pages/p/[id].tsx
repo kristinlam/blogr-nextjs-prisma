56569b904cd2881c87151059d33bf9b7c9393055
Kristin Lam <klam623@gmail.com>
2023-03-30 22:58:27 -0400

    Add delete functionality

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -29,6 +29,11 @@ async function publishPost(id: string): Promise<void> {
   await Router.push('/');
 }
 
+async function deletePost(id: string): Promise<void> {
+  await axios.delete(`/api/post/${id}`);
+  await Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
   const { data: session, status } = useSession();
   if (status === 'loading') {
@@ -49,6 +54,9 @@ const Post: React.FC<PostProps> = (props) => {
         <ReactMarkdown children={props.content} />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(props.id)}>Delete</button>
         )}
       </div>
       <style jsx>{`
